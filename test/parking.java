50089513602b62fb23644c49fc465d41765b95f1
Taylor Xia <tyxia@stu.naperville203.org>
2018-09-10 12:12:58 -0500

    Start parking program. Almost done. There's still some issues with removing cars.

--- test/parking.java
+++ test/parking.java
@@ -1,33 +1,68 @@
+import java.util.*;
 
 /**
- * Write a description of class parking here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
+ * Parking Program
  */
 public class parking
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class parking
-     */
-    public parking()
+    public static void main(String[] args)
     {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+        Stack<Integer> street = new Stack<>();
+        Stack<Integer> park = new Stack<>();
+        Scanner input = new Scanner(System.in);
+        int check;
+        
+        System.out.println("Please Add/Remove a Vehicle (Enter 0 to exit)");
+        int car = input.nextInt();
+        while (car != 0)
+        {
+            //Situation if adding a vehicle
+            if (car > 0)
+            {
+                //Check is element is already in list. Will return element position if it is and -1 if it isn't
+                check = park.search(car);
+                if (check != -1)
+                {
+                    System.out.println("Car is already in driveway!");
+                    System.out.println("Please Add/Remove a Vehicle (Enter 0 to exit)");
+                    car = input.nextInt();
+                }
+                else
+                {
+                    park.push(car);
+                    System.out.println("Driveway:\t"+Arrays.toString(park.toArray()));
+                    System.out.println("Street:\t"+Arrays.toString(street.toArray()));
+                    System.out.println("Vehicle Added Successfully!");
+                    System.out.println("Please Add/Remove a Vehicle (Enter 0 to exit)");
+                    car = input.nextInt();
+                }
+            }
+            
+            //Situation if removing a vehicle
+            if (car < 0)
+            {
+                //Checking if car even exists
+                check = park.search(car);
+                if (check == -1)
+                {
+                    System.out.println("Car is not in driveway!");
+                    System.out.println("Please Add/Remove a Vehicle (Enter 0 to exit)");
+                    car = input.nextInt();
+                }
+                else
+                {   //Pops every car and adds it to the street until it reaches the car it's looking for
+                    while (park.peek() != car)
+                    {
+                        street.push(park.pop());
+                    }
+                    park.pop();
+                    System.out.println("Driveway:\t"+Arrays.toString(park.toArray()));
+                    System.out.println("Street:\t"+Arrays.toString(street.toArray()));
+                    System.out.println("Vehicle Removed Successfully!");
+                    System.out.println("Please Add/Remove a Vehicle (Enter 0 to exit)");
+                    car = input.nextInt();
+                }
+            }
+        }                    
     }
 }
