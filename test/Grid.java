93e6ccceec2c098888bb0abffce48b1fd4f1dd57
Taylor Xia <tyxia@stu.naperville203.org>
2018-09-13 12:20:23 -0500

    Started with flood fill. Need to still do some work

--- test/Grid.java
+++ test/Grid.java
@@ -1,18 +1,53 @@
+import java.util.*;
+
 public class Grid
 {
    private static final int SIZE = 10;
    int[][] pixels = new int[SIZE][SIZE];
-   . . .
+   Stack neighbors = new Stack();
+   int start = 1;
 
    /**
       Flood fill, starting with the given row and column.
    */
    public void floodfill(int row, int column)
    {
-      . . .
+      if (pixels[row][column+1] != 0 || pixels[row+1][column] != 0 || pixels[row-1][column] != 0 || pixels[row][column-1] != 0)
+        System.out.println("All pixels filled!");
+      else
+      {
+          //Checks each of the pixels neighbors if they equal zero and if it's not at the edge of the array
+          pixels[row][column] = start;
+          neighbors.push(start);
+          start++;
+          if (pixels[row+1][column] == 0 && row != 0)
+          {
+              pixels[row+1][column] = start;
+              neighbors.push(start);
+              start++;
+          }
+          if (pixels[row][column+1] == 0 && column != 10)
+          {
+              pixels[row][column+1] = start;
+              neighbors.push(start);
+              start++;
+          }
+          if (pixels[row-1][column] == 0 && row != 10)
+          {
+              pixels[row][column+1] = start;
+              neighbors.push(start);
+              start++;
+          }
+          if (pixels[row][column-1] == 0 && column != 0)
+          {
+              pixels[row][column+1] = start;
+              neighbors.push(start);
+              start++;
+          }
+          start = neightbors.pop();
+      }
    }
 
-   @Override
    public String toString()
    {
       String r = "";
